817fa344cff9d9af5735aa252754d70439c76534
Alejandro Rodríguez <56249863+alexrrlm@users.noreply.github.com>
2020-02-29 15:21:30 +0100

    Alejandro - Clase partida terminada.

--- Diez-Mil/clase_partida.py
+++ Diez-Mil/clase_partida.py
@@ -2,15 +2,13 @@
 Clase para jugar al juego de dados del DiezMil:
     Hará uso de la clase jugador.
 """
-from clase_jugadores import Jugador
+from Clase_jugadores import Clase_jugadores
+import Clase_jugadores
 
 class Partida:
     """
     Clase que implementa la partida para el DiezMil:
         Reglas: http://www.acanomas.com/Reglamentos-Juegos-de-Dados/1235/Diez-mil.htm
-    Variables de instancia:
-        * __jugadores: tupla con objetos de clase Jugador (mínimo 4).
-        *
 
     Métodos:
         * jugar: ejecuta el juego.
@@ -18,8 +16,8 @@ class Partida:
     """
 
     def __init__(self, *jugadores):
-        self.__jugadores
-        self.__turno = 0
+        self.__jugadores = jugadores
+
 
     def jugar(self):
         # Inicio Partida
@@ -30,18 +28,56 @@ class Partida:
         self.jugar_turno()
 
         # Imprimir puntuación
-
+        self.__salida_pantalla(marcador)
+    def jugar_ronda(self):
+        pass
 
     def jugar_turno(self):
-        self.__turno += 1
+        bool = True
+        while bool:
+            Clase_jugadores.reset()
+            jugar_turno_jugador()
+            if Jugador.puntos_acumulados == 10000:
+                print(f"Felicidades {Clase_jugadores} has ganado.")
+                self.__salida_pantalla(marcador)
+                break
+            if Jugador.puntos_acumulados > 10000:
+                print(f"Lo siento {Clase_jugadores} has perdido.")
+                self.__salida_pantalla(marcador)
+                break
+            n = raw_input("Quieres seguir tirando (s/n):")
+            if n.strip() == 'n':
+                bool = False
+
+        pass
+
+    def jugar_turno_jugador(self):
+        j.comprobar_puntos()
+        self.__salida_pantalla(Partida.salida_tirada(j))
+        pass
+
+    def salida_tirada(j):
+        print("Ha salido: ")
+        for i in Clase_jugadores.dados:
+            print(Clase_jugadores.dados)
+        print(f"Tienes {Clase_jugadores.puntos_turno()}.")
+        print(f"Tus puntos totales son: {Clase_jugadores.puntos_acumulados()}.")
 
 
 
     def __salida_pantalla(self, salida):
+        self.__salida[self.turno] += salida + "\n"
+
+    def marcador(self):
+        print("--Marcador--")
+        print(f"Jugador 1: {p[0]}")
+        print(f"Jugador 2: {p[1]}")
+        print(f"Jugador 3: {p[2]}")
+        print(f"Jugador 4: {p[3]}")
 
 
-n_jugadores = int(input("Introduce el número de jugadores: "))
-for i in range(n_jugadores):
-    p += Jugador("Jugador"[i],)
+p = Partida(Clase_jugadores("Jugador1"), Clase_jugadores("Jugador2"), Clase_jugadores("Jugador3"), Clase_jugadores("Jugador4"))
+
 p.jugar()
-print(p)
+
+
